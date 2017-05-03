7469d9331b13a72561aa39e0e6b1cbd752cc3451
W.D <option0417@gmail.com>
2017-05-04 00:16:33 +0800

    Update 88_merge_sorted_array.java

--- leetcode/88_merge_sorted_array.java
+++ leetcode/88_merge_sorted_array.java
@@ -1,5 +1,23 @@
 public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int tail = m + n - 1;
+        m--;
+        n--;
+        
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[tail--] = nums1[m--];
+            } else {
+                nums1[tail--] = nums2[n--];
+            }
+        }
+        
+        while (n >= 0) {
+            nums1[tail--] = nums2[n--];
+        }
+    }
+    
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
         int num1Idx = 0;
         int num2Idx = 0;
         int idx = 0;
