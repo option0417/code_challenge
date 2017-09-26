096eb048a6c63812048fc245a490609629cd2919
W.D Su <option0417@gmail.com>
2017-09-26 03:55:05 +0000

    Add comment

--- leetcode/7_Reverse_Integer.c
+++ leetcode/7_Reverse_Integer.c
@@ -1,17 +1,23 @@
 #include "stdio.h"
 
+/*
+ * Source: https://leetcode.com/problems/reverse-integer/description/
+ * Author: W.D Su
+ * Date:   09/27/2017
+*/
+
 int reverse(int );
 
 int main(int argc, char* argv[]) {
-	printf("%d\n", reverse(123));			// 321
-	printf("%d\n", reverse(-123));			// -321
-	printf("%d\n", reverse(2147483647));	// 0
-	printf("%d\n", reverse(-2147483647));	// 0
-	printf("%d\n", reverse(1463847412));	// 2147483641
-	printf("%d\n", reverse(-1463847412));	// -2147483641
-	printf("%d\n", reverse(1000000003));	// 0
-	printf("%d\n", reverse(-1000000003));	// 0
-	return 0;
+  printf("%d\n", reverse(123));         // 321
+  printf("%d\n", reverse(-123));        // -321
+  printf("%d\n", reverse(2147483647));  // 0
+  printf("%d\n", reverse(-2147483647)); // 0
+  printf("%d\n", reverse(1463847412));  // 2147483641
+  printf("%d\n", reverse(-1463847412)); // -2147483641
+  printf("%d\n", reverse(1000000003));  // 0
+  printf("%d\n", reverse(-1000000003)); // 0
+  return 0;
 }
 
 int reverse(int x) {
