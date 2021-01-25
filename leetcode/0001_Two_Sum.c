e870ef623843b3bd8d6abe1003ebedd173463da3
option0417 <option0417@gmail.com>
2021-01-25 23:34:47 +0800

    Let 0001_Two_Sum executable

--- leetcode/0001_Two_Sum.c
+++ leetcode/0001_Two_Sum.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
@@ -20,7 +21,32 @@ int* twoSum(int* nums, int numsSize, int target) {
     return NULL;
 }
 
-void main(void) {
+int main(int argc, char* argv[]) {
+  int nums[5] = {1, 2, 3, 4, 5};
+  int* result; 
   
-  return;
+  result = twoSum(nums, 5, 3);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 0, 1
+  
+  result = twoSum(nums, 5, 4);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 0, 2
+  
+  result = twoSum(nums, 5, 5);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 0, 3
+  
+  result = twoSum(nums, 5, 6);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 0, 4
+  
+  result = twoSum(nums, 5, 7);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 1, 4
+  
+  result = twoSum(nums, 5, 8);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 2, 4
+  
+  result = twoSum(nums, 5, 9);
+  printf("Result: %d, %d\n", result[0], result[1]);  // 3, 4
+
+  result = twoSum(nums, 5, 10);
+  printf("Result is Null: %s\n", result == NULL ? "true" : "false");    // NULL
+  return 0;
 }
